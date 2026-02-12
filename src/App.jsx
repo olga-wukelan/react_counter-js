5fbded2a862c5d0a27c32c078dfb517e1eaa61ad
Olga Wukelan <olga.wukelan@gmail.com>
2026-02-12 11:46:31 +0400

    Fix code in increase logic

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,16 @@ export const App = () => {
     setCount(prev => prev + 100);
   };
 
-  // DON'T change the code below
   const increase = () => {
-    if (count % 5 === 0) {
-      add100();
-    }
+    setCount(prev => {
+      const next = prev + 1;
 
-    addOne();
-  };
+      if (next % 5 === 0) {
+        return next + 100;
+      }
+      return next;
+    });
+}
 
   return (
     <div className="App">
